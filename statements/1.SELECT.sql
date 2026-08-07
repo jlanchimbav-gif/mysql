8b103cbc6113968533c5238394f4c6be608b6c6d
Jaguar Dev <dev@jaguar-web.local>
2026-08-06 20:26:14 -0500

    Main

---
+++ statements/1.SELECT.sql
@@ -0,0 +1,4 @@
+## SE usa para seleccionar datos de la base de datos
+SELECT * FROM users;
+SELECT NAME FROM users;
+SELECT user_id, name FROM users;
---
+++ statements/5.LIKE.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE name LIKE '%John%';
+
+SELECT * FROM users WHERE email LIKE '%@gmail.com';
+
+SELECT * FROM users WHERE email LIKE 'sara%' AND age > 18;
